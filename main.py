30f2b865c29ea0c043a03ae3ee712f255c731144
MrFlava <thatelitemaili33t@gmail.com>
2022-04-19 13:20:27 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import graphene
+
+#TODO:
+# 2. Create User and Post mdoels;
+# 3. Create auth;
+# 4. Create CRUD endpoints for Post model
+# 5. Create Readme.md
+
+
+class Query(graphene.ObjectType):
+  hello = graphene.String(name=graphene.String(default_value="World"))
+
+  def resolve_hello(self, info, name):
+    return 'Hello ' + name
+
+
+schema = graphene.Schema(query=Query)
+result = schema.execute('{ hello }')
+print(result.data['hello'])
