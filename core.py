8c0bddf5b81c78241e26e88bbab5f4abeba48aa7
MrFlava <thatelitemaili33t@gmail.com>
2025-02-08 16:45:30 +0200

    Work on the core.py

--- core.py
+++ core.py
@@ -1,6 +1,9 @@
-from typing import  List, Optional
-
 import strawberry
 
-from controller import CreateMutation, Queries
+from controller import CreateMutation
 from db.schemas import UserType, PostType
+
+@strawberry.type
+class Mutation:
+    add_user: UserType = strawberry.mutation(resolver=CreateMutation.add_user)
+    add_post: PostType = strawberry.mutation(resolver=CreateMutation.add_post)
